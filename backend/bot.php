c5bc699fed4641f4452a8138b9b58e8a395c974d
mackliu <macklun@gmail.com>
2020-07-29 15:28:53 +0800

    完成後台頁尾版權功能

---
+++ backend/bot.php
@@ -0,0 +1,19 @@
+<?php
+if(!empty($_POST['bottom'])){
+    $bot=$Bottom->find(1);
+    $bot['bottom']=$_POST['bottom'];
+    $Bottom->save($bot);
+}
+
+?>
+
+<h2 class="ct">編輯頁尾版權區</h2>
+<form action="?do=bot" method="post">
+    <table class="all">
+        <tr>
+            <td class="tt">頁尾宣告內容</td>
+            <td class="pp"><input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>"></td>
+        </tr>
+    </table>
+    <div class="ct"><input type="submit" value="編輯"><input type="button" value="重置" onclick="javascript:$('input[name=\'bottom\']').val('')"></div>
+</form>
